1ebb4f9d526666e85b58d6035e9b2cc5aefbf59a
sn0w0b <167842670+sn0w0b@users.noreply.github.com>
2025-02-16 21:28:39 -0600

    Add files via upload
    Print the square version on a new line

---
+++ LoopButSquare.py
@@ -0,0 +1,9 @@
+#Martha Moreno Gonzalez
+#2/16/2025
+#will print out the squared versio of thew list of numbers on a new line
+
+numbers = [12, 10, 32, 3, 66, 17, 42, 99, 20]
+
+for number in numbers:
+    print(f"{number} - {number ** 2}") 
+
